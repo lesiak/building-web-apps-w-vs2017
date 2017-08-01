f8d04e4a2f20029dde6553db30b1203d6dbc75d0
lesiak <lesiak@outlook.com>
2017-08-02 00:17:18 +0200

    Added StoreContext

---
+++ Chapter01/SpyStore.DAL/SpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpyStore.DAL.EF
+{
+    class StoreContext : DbContext
+    {
+        public StoreContext()
+        {
+        }
+
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
