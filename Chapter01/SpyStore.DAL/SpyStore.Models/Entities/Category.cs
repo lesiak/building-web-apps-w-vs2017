03fc561bc02e5121b2d8daf6713fb13b96c1a233
lesiak <lesiak@outlook.com>
2017-08-06 01:06:07 +0200

    Added annotations to Category class

--- Chapter01/SpyStore.DAL/SpyStore.Models/Entities/Category.cs
+++ Chapter01/SpyStore.DAL/SpyStore.Models/Entities/Category.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using SpyStore.Models.Entities.Base;
 
 namespace SpyStore.Models.Entities
 {
+    [Table("Categories", Schema = "Store")]
     public class Category : EntityBase
     {
         [DataType(DataType.Text), MaxLength(50)]
