0ee5ff639a327008e6c0ab43f07ecc21cff2af5d
lesiak <lesiak@outlook.com>
2017-08-09 11:21:29 +0200

    Added db migration

--- Chapter02/SpyStore.DAL/SpyStore.DAL/EF/Migrations/20170809091734_RemainingTables.Designer.cs
+++ Chapter02/SpyStore.DAL/SpyStore.DAL/EF/Migrations/20170809091734_RemainingTables.Designer.cs
@@ -8,9 +8,10 @@ using SpyStore.DAL.EF;
 namespace SpyStore.DAL.EF.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20170809091734_RemainingTables")]
+    partial class RemainingTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
