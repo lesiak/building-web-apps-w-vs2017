415d350efe33ddd7bb817dafd3a47575bae4e1eb
lesiak <lesiak@outlook.com>
2017-08-11 13:30:23 +0200

    Added Controllers

---
+++ Chapter03/SpyStore.Service/SpyStore.Service/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using SpyStore.DAL.Repos.Interfaces;
+
+namespace SpyStore.Service.Controllers
+{
+    [Route("api/[controller]/{customerId}")]
+    public class OrdersController : Controller
+    {
+        private IOrderRepo Repo { get; set; }
+        public OrdersController(IOrderRepo repo)
+        {
+            Repo = repo;
+        }
+
+        public IActionResult GetOrderHistory(int customerId)
+        {
+            //No modifications to the route - no id param required
+            var orderWithTotals = Repo.GetOrderHistory(customerId);
+            return orderWithTotals == null ? (IActionResult)NotFound() : new ObjectResult(orderWithTotals);
+        }
+
+        [HttpGet("{orderId}", Name = "GetOrderDetails")]
+        public IActionResult GetOrderForCustomer(int customerId, int orderId)
+        {
+            var orderWithDetails = Repo.GetOneWithDetails(customerId, orderId);
+            return orderWithDetails == null ? (IActionResult)NotFound() : new ObjectResult(orderWithDetails);
+        }
+    }
+}
