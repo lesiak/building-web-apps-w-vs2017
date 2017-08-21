8d722b124ba5bcd5d3dbdbc79c0009dc9b3e20e6
lesiak <lesiak@outlook.com>
2017-08-21 15:37:27 +0200

    Applied validation

--- Chapter05/SpyStore.MVC/SpyStore.MVC/ViewModels/AddToCartViewModel.cs
+++ Chapter05/SpyStore.MVC/SpyStore.MVC/ViewModels/AddToCartViewModel.cs
@@ -1,9 +1,11 @@
-﻿using SpyStore.MVC.ViewModels.Base;
+﻿using SpyStore.MVC.Validation;
+using SpyStore.MVC.ViewModels.Base;
 
 namespace SpyStore.MVC.ViewModels
 {
     public class AddToCartViewModel :CartViewModelBase
     {
+        [MustNotBeGreaterThan(nameof(UnitsInStock)), MustBeGreaterThanZero]
         public int Quantity { get; set; }
     }
 }
--- Chapter05/SpyStore.MVC/SpyStore.MVC/ViewModels/CartRecordViewModel.cs
+++ Chapter05/SpyStore.MVC/SpyStore.MVC/ViewModels/CartRecordViewModel.cs
@@ -1,9 +1,11 @@
-﻿using SpyStore.MVC.ViewModels.Base;
+﻿using SpyStore.MVC.Validation;
+using SpyStore.MVC.ViewModels.Base;
 
 namespace SpyStore.MVC.ViewModels
 {
     public class CartRecordViewModel : CartViewModelBase
     {
+        [MustNotBeGreaterThan(nameof(UnitsInStock))]
         public int Quantity { get; set; }
     }
 }
